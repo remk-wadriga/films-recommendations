7d6334b2c22b5624742c67c527f082f13a66c4b5
Rem <dkushneriov@aurum-soft.com>
2019-08-01 22:02:29 +0300

    Backend:     Create Languages enum type     Create fixtures for films

--- src/DataFixtures/FilmFixtures.php
+++ src/DataFixtures/FilmFixtures.php
@@ -3,6 +3,15 @@
 namespace App\DataFixtures;
 
 use App\Entity\Actor;
+use App\Entity\Company;
+use App\Entity\Director;
+use App\Entity\Film;
+use App\Entity\Genre;
+use App\Entity\Premium;
+use App\Entity\Producer;
+use App\Entity\Types\Enum\LanguagesEnum;
+use App\Entity\User;
+use App\Entity\Writer;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -12,12 +21,162 @@ class FilmFixtures extends AbstractFixture implements DependentFixtureInterface
 
     public function loadData(ObjectManager $manager)
     {
-        $actor = $this->getRandomReference(Actor::class);
-        dd($actor);
+        $languages = LanguagesEnum::getAvailableTypes();
 
-        //dd('OK');
+        $this->createMany(Film::class, 10000, function (Film $film, int $i) use ($languages) {
+            /** @var User $user */
+            /** @var Genre[] $genres */
+            /** @var Company[] $companies */
+            /** @var Director[] $directors */
+            /** @var Actor[] $actors */
+            /** @var Producer[] $producers */
+            /** @var Writer[] $writers */
+            /** @var Premium[] $premiums */
+            $user = $this->getRandomReference(User::class);
+            $genres = $this->getRandomReferences(Genre::class, $this->faker->numberBetween(1, 3));
+            $companies = $this->getRandomReferences(Company::class, $this->faker->numberBetween(1, 5));
+            $directorsCount = $this->faker->numberBetween(1, 5) === 1 ? $this->faker->numberBetween(1, 3) : 1;
+            $directors = $this->getRandomReferences(Director::class, $directorsCount);
+            $actors = $this->getRandomReferences(Actor::class, $this->faker->numberBetween(1, 15));
+            $producersCount = $this->faker->numberBetween(1, 5) === 1 ? $this->faker->numberBetween(1, 3) : 1;
+            $producers = $this->getRandomReferences(Producer::class, $producersCount);
+            $writersCount = $this->faker->numberBetween(1, 5) === 1 ? $this->faker->numberBetween(1, 3) : 1;
+            $writers = $this->getRandomReferences(Writer::class, $writersCount);
+            $premiumsCount = $this->faker->numberBetween(1, 20) === 1 ? $this->faker->numberBetween(0, 3) : 0;
+            $premiums = $this->getRandomReferences(Premium::class, $premiumsCount);
 
-        //$manager->flush();
+            foreach ($genres as $genre) {
+                $film->addGenre($genre);
+            }
+            foreach ($companies as $company) {
+                $film->addCompany($company);
+            }
+            foreach ($directors as $director) {
+                $film->addDirector($director);
+            }
+            foreach ($actors as $actor) {
+                $film->addActor($actor);
+            }
+            foreach ($producers as $producer) {
+                $film->addProducer($producer);
+            }
+            foreach ($writers as $writer) {
+                $film->addWriter($writer);
+            }
+            foreach ($premiums as $premium) {
+                $film->addPremium($premium);
+            }
+
+            $name = $this->faker->name;
+            if ($this->faker->numberBetween(1, 2) === 1) {
+                $name .= ' ' . $this->faker->name;
+                if ($this->faker->numberBetween(1, 2) === 1) {
+                    $name .= ' ' . $this->faker->name;
+                    if ($this->faker->numberBetween(1, 2) === 1) {
+                        $name .= ' ' . $this->faker->name;
+                    }
+                }
+            }
+
+            $budget = $this->faker->numberBetween(1000, 500000000);
+            $sales = $budget;
+            if ($this->faker->numberBetween(1, 3) === 1) {
+                if ($this->faker->numberBetween(1, 3) === 1) {
+                    $sales *= $this->faker->numberBetween(1, 15);
+                } else {
+                    $sales += $this->faker->numberBetween(1, 100000000);
+                }
+            } else {
+                if ($this->faker->numberBetween(1, 3) === 1) {
+                    $sales /= $this->faker->numberBetween(1, 15);
+                } else {
+                    $sales -= $this->faker->numberBetween(1, $sales);
+                }
+            }
+
+            $filmLanguages = [];
+            if ($this->faker->numberBetween(1, 5) === 1) {
+                $filmLanguages[] = LanguagesEnum::ENGLISH;
+            }
+            if ($this->faker->numberBetween(1, 15) === 1) {
+                $filmLanguages[] = LanguagesEnum::RUSSIAN;
+            }
+            if ($this->faker->numberBetween(1, 20) === 1) {
+                $filmLanguages[] = LanguagesEnum::FRENCH;
+            }
+            if ($this->faker->numberBetween(1, 20) === 1) {
+                $filmLanguages[] = LanguagesEnum::ITALIAN;
+            }
+            if (empty($filmLanguages) || $this->faker->numberBetween(1, 10) === 1) {
+                $firstLanguage = $this->faker->randomElement($languages);
+                if (!in_array($firstLanguage, $filmLanguages)) {
+                    $filmLanguages[] = $firstLanguage;
+                }
+                if ($this->faker->numberBetween(1, 3) === 1) {
+                    $secondLanguage = $this->faker->randomElement($languages);
+                    if (!in_array($secondLanguage, $filmLanguages)) {
+                        $filmLanguages[] = $secondLanguage;
+                    }
+                    if ($this->faker->numberBetween(1, 3) === 1) {
+                        $thirdLanguage = $this->faker->randomElement($languages);
+                        if (!in_array($thirdLanguage, $filmLanguages)) {
+                            $filmLanguages[] = $thirdLanguage;
+                        }
+                    }
+                }
+            }
+
+            $date = null;
+            if ($this->faker->numberBetween(1, 10) === 1) {
+                $date = $this->faker->dateTimeBetween('-20 years');
+            }
+            if ($date === null && $this->faker->numberBetween(1, 15) === 1) {
+                $date = $this->faker->dateTimeBetween('-40 years');
+            }
+            if ($date === null && $this->faker->numberBetween(1, 20) === 1) {
+                $date = $this->faker->dateTimeBetween('-60 years');
+            }
+            if ($date === null && $this->faker->numberBetween(1, 25) === 1) {
+                $date = $this->faker->dateTimeBetween('-80 years');
+            }
+            if ($date === null && $this->faker->numberBetween(1, 30) === 1) {
+                $date = $this->faker->dateTimeBetween('-100 years');
+            }
+            if ($date === null && $this->faker->numberBetween(1, 40) === 1) {
+                $date = $this->faker->dateTimeBetween('-120 years');
+            }
+            if ($date === null) {
+                $date = $this->faker->dateTimeBetween('-10 years');
+            }
+
+            $duration = 0;
+            if ($this->faker->numberBetween(1, 2) === 1) {
+                $duration = $this->faker->numberBetween(75, 120);
+            }
+            if ($duration === 0 && $this->faker->numberBetween(1, 3) === 1) {
+                $duration = $this->faker->numberBetween(60, 180);
+            }
+            if ($duration === 0 && $this->faker->numberBetween(1, 4) === 1) {
+                $duration = $this->faker->numberBetween(180, 1000);
+            }
+            if ($duration === 0) {
+                $duration = $this->faker->numberBetween(75, 120);
+            }
+
+            $film
+                ->setUser($user)
+                ->setName($name)
+                ->setDescription($this->faker->text)
+                ->setPoster('/img/poster/default_poster.jpg')
+                ->setBudget($budget)
+                ->setSales((int)$sales)
+                ->setLanguages($filmLanguages)
+                ->setDate($date)
+                ->setDuration($duration)
+            ;
+        });
+
+        $manager->flush();
     }
 
     public function getDependencies()
